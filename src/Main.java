c84402283757b73dd7442a7e9cafcfcf0575c75f
vinayak paimode <vinayakpaimode61382gmail.com>
2023-02-08 23:25:12 +0530

    commit by vinayak paimode

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+//Pascle nameing convention  use for class name
+// for eg : VinayakBalasahebPaimode
+
+//Camel case convention use for main method
+// for eg : vinayakBalasahebPaimode
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! 456ffffff325");
+    }
+}
+
+
+
